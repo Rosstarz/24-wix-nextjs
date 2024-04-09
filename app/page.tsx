54c55456a3e4c48646428c1070aee4e68da8faa2
arti <56412092+Rosstarz@users.noreply.github.com>
2024-04-09 21:56:48 +0000

    i

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,54 @@
-import React from "react";
-import HeroSection from "@/app/components/HomePage/Hero/Hero";
-import AboutSection from "@/app/components/HomePage/About/About";
-import MenuSection from "@/app/components/HomePage/Menu/Menu";
-import Testimonies from "@/app/components/HomePage/Testimonies/Testimonies";
-import ServiceForm from "@/app/components/HomePage/Form/ServiceForm";
-import HomeGallery from "@/app/components/HomePage/HomeGallery/HomeGallery";
-import { BlogSection } from "@/app/components/HomePage/BlogSection/BlogSection";
-import FeaturedMenuCard from "@/app/components/MenuCard/FeaturedMenuCard";
-import { StoreSection } from "@/app/components/HomePage/StoreSection/StoreSection";
+import React from 'react';
+import HeroSection from '@/app/components/HomePage/Hero/Hero';
+import AboutSection from '@/app/components/HomePage/About/About';
+import MenuSection from '@/app/components/HomePage/Menu/Menu';
+import Testimonies from '@/app/components/HomePage/Testimonies/Testimonies';
+import ServiceForm from '@/app/components/HomePage/Form/ServiceForm';
+import HomeGallery from '@/app/components/HomePage/HomeGallery/HomeGallery';
+import { BlogSection } from '@/app/components/HomePage/BlogSection/BlogSection';
+import FeaturedMenuCard from '@/app/components/MenuCard/FeaturedMenuCard';
+import { StoreSection } from '@/app/components/HomePage/StoreSection/StoreSection';
 
 const galleryImages = [
   {
     id: 1,
-    src: "/images/gallery-image1.jpeg",
-    title: "Im an image title",
+    src: '/images/gallery-image1.jpeg',
+    title: 'Im an image title',
     description:
-      "Describe your image here. Use catchy text to tell people the story behind the photo. Go to “Manage Media” to add your content.",
+      'Describe your image here. Use catchy text to tell people the story behind the photo. Go to “Manage Media” to add your content.',
   },
   {
     id: 2,
-    src: "/images/gallery-image2.jpeg",
-    title: "Im an image title",
+    src: '/images/gallery-image2.jpeg',
+    title: 'Im an image title',
     description:
-      "Describe your image here. Use catchy text to tell people the story behind the photo. Go to “Manage Media” to add your content.",
+      'Describe your image here. Use catchy text to tell people the story behind the photo. Go to “Manage Media” to add your content.',
   },
   {
     id: 3,
-    src: "/images/gallery-image3.jpeg",
-    title: "Im an image title",
+    src: '/images/gallery-image3.jpeg',
+    title: 'Im an image title',
     description:
-      "Describe your image here. Use catchy text to tell people the story behind the photo. Go to “Manage Media” to add your content.",
+      'Describe your image here. Use catchy text to tell people the story behind the photo. Go to “Manage Media” to add your content.',
   },
   {
     id: 4,
-    src: "/images/gallery-image4.jpeg",
-    title: "Im an image title",
+    src: '/images/gallery-image4.jpeg',
+    title: 'Im an image title',
     description:
-      "Describe your image here. Use catchy text to tell people the story behind the photo. Go to “Manage Media” to add your content.",
+      'Describe your image here. Use catchy text to tell people the story behind the photo. Go to “Manage Media” to add your content.',
   },
 ];
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <HeroSection />
+      
+    </main>
+  );
+}
+
+{/* <HeroSection />
       <AboutSection />
       <MenuSection />
       <StoreSection />
@@ -59,7 +64,4 @@ export default function Home() {
       />
       <BlogSection />
       <ServiceForm />
-      <HomeGallery images={galleryImages} />
-    </main>
-  );
-}
+      <HomeGallery images={galleryImages} /> */}
